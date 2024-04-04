3eebf485c85f813d188e31d06a79699dddc0a8db
mvk24 <varun.mithyantha@gmail.com>
2024-04-04 12:14:25 +0530

    Final Commit, Ready to launch

---
+++ MyEcommerceAdmin/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyEcommerceAdmin.Models;
+namespace MyEcommerceAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyEcommerceDbContext db = new MyEcommerceDbContext();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.VegProduct = db.Products.Where(x => x.Category.Name.Equals("Vegitables")).ToList();
+            ViewBag.FruitProduct = db.Products.Where(x => x.Category.Name.Equals("Fruits")).ToList();
+            ViewBag.DairyProduct = db.Products.Where(x => x.Category.Name.Equals("Dairy Products")).ToList();
+            ViewBag.PickProduct = db.Products.Where(x => x.Category.Name.Equals("Pickles")).ToList();
+            ViewBag.Slider = db.genMainSliders.ToList();
+            ViewBag.PromoRight = db.genPromoRights.ToList();
+
+            this.GetDefaultData();
+
+            return View();
+        }
+    }
+}
---
+++ MyEcommerceAdmin/Models/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyEcommerceAdmin.Models
+{
+    public class ViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<SubCategory> SubCategories { get; set; }
+    }
+}
